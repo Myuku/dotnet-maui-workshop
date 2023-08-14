8240458a5475d976b4b9d410632cce6710a883a4
Myuku <junelee2002jh@gmail.com>
2023-08-14 16:49:36 -0700

    Implemented Monkey details page

--- Part 1 - Displaying Data/MonkeyFinder/ViewModel/MonkeyDetailsViewModel.cs
+++ Part 1 - Displaying Data/MonkeyFinder/ViewModel/MonkeyDetailsViewModel.cs
@@ -1,5 +1,18 @@
 ﻿namespace MonkeyFinder.ViewModel;
 
+[QueryProperty("Monkey", "Monkey")]
 public partial class MonkeyDetailsViewModel : BaseViewModel
 {
+    public MonkeyDetailsViewModel() 
+    { 
+    }
+
+    [ObservableProperty]
+    Monkey monkey;
+
+    [RelayCommand]
+    async Task GoBackAsync()
+    {
+        await Shell.Current.GoToAsync("..");
+    }
 }
